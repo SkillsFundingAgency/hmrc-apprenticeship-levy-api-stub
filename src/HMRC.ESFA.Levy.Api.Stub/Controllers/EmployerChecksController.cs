b8ccedab17ac1e7b5a36478d1233a4357c689d8c
David Scuffle <david.scuffle1@accomplish-it.co.uk>
2018-01-22 13:37:29 +0000

    Removed EmpRef Decode

--- src/HMRC.ESFA.Levy.Api.Stub/Controllers/EmployerChecksController.cs
+++ src/HMRC.ESFA.Levy.Api.Stub/Controllers/EmployerChecksController.cs
@@ -27,8 +27,6 @@ namespace HMRC.ESFA.Levy.Api.Stub.Controllers
         public async Task<EmploymentStatus> GetEmploymentStatus(string empRef, string nino, DateTime? fromDate = null,
             DateTime? toDate = null)
         {
-            empRef = HttpUtility.UrlDecode(empRef);
-
             return await Repository.GetEmploymentStatus(empRef, nino, fromDate, toDate);
         }
     }
